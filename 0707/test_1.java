077349c5589820fb9d8a5a2fdb99cd08f692b307
rosejinhui <108884226+rosejinhui@users.noreply.github.com>
2022-07-07 23:47:21 +0900

    Add file

---
+++ 0707/test_1.java
@@ -0,0 +1,18 @@
+package class_test;
+
+public class test_1 {
+
+	public static void main(String[] args) {
+		
+//		1. 다음과 같은 내용을 출력하는 코드를 작성하시오.
+//		sorry~~~~
+//		재밌어서 미안합니다~~~
+//		잘해서 미안합니다~~
+		//---------------------------------------------------------
+		
+		System.out.println("sorry~~~");
+		System.out.println("재밌어서 미안합니다~~");
+		System.out.println("잘해서 미안합니다~~");
+	}
+
+}
